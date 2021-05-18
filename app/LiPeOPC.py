c5b54d7e355bddbdfd4384bd46b599fe126b0b5e
benedikt97 <Heuser@live.de>
2021-05-18 12:22:09 +0200

    Implemented static/cyclic observation

--- app/LiPeOPC.py
+++ app/LiPeOPC.py
@@ -11,32 +11,41 @@ from threading import Thread
 sys.path.insert(0, "..")
 
 class opccoll(Thread):
-    def __init__(self, opcconfigjs, nodesjs, opcclient, dbconn, isLogging):
+    def __init__(self, opcconfigjs, nodesjs, opcclient, dbconn):
         Thread.__init__(self)
-        self.isLogging = isLogging
+        self.isLogging = opcconfigjs["DBLogging"]
         self.configjs = opcconfigjs
-        self.nodesjs = nodesjs
+        self.nodesjs = nodesjs["cyclic"]
+        self.staticnodesjs = nodesjs["static"]
         self.client = opcclient
         self.dbconn = dbconn    
         #Prepare Database Table and get SQL Pre  
         try:
-            if isLogging:
+            if self.isLogging:
                 self. sqlpre = self.dbconn.prepareAndSelectLoggingTable(self.configjs["DBTablename"], self.nodesjs)        
             self.dbconn.writecom("opc", "Message", "Table in Database created", "")
         except Exception as e:
             self.dbconn.writecom("opc", "Error", "No Connection to Database possible", str(e))
-        return   
+        #get Fixed Values
+        internActValues = {}
+        for id in self.staticnodesjs:          
+            node = self.client.get_node(self.staticnodesjs[id])
+            value = node.get_value()     
+            internActValues[id] = value   
+        self.staticValues = internActValues
+        return
+
 
     def run(self):
         self.isRunning = True
-        while self.isRunning:
-            self.internActValues = {}       
+        while self.isRunning:       
             try:
+                internActValues = {}
                 for id in self.nodesjs:          
                     node = self.client.get_node(self.nodesjs[id])
                     value = node.get_value()     
-                    self.internActValues[id] = value   
-                self.actValues = self.internActValues    
+                    internActValues[id] = value   
+                self.actValues = internActValues    
                 if self.isLogging:
                     self.dbconn.WriteActValues(self.sqlpre, self.actValues)      
                 time.sleep(self.configjs["Zyklus"])  
@@ -47,14 +56,15 @@ class opccoll(Thread):
 
 class opccon:
     
-    def __init__(self, opcconfigjs, nodesjs):
+    def __init__(self):
         self.actValues = {}
         #Connect to Database
         self.dbconn = lpd.dbcon("LiPe", "127.0.0.1", "opc", "LiPePWD320")
         return         
 
-    def initopc(self, opcconfigjs, nodesjs, isLogging):
+    def initOpc(self, opcconfigjs, nodesjs):
         #Check if Thread is already running
+        
         try:
             if self.opcthread.isAlive():
                 self.dbconn.writecom("opc", "Error", "Thread is running, please stop Thread for new Configuration", "")
@@ -72,25 +82,14 @@ class opccon:
             self.dbconn.writecom("opc", "Error", "No Connection to OPC Server", str(e))
             return
         #Start OPC Collecting Thread
-        self.isLogging = isLogging
-        self.opcthread = opccoll(self.configjs, self.nodesjs, self.client, self.dbconn, self.isLogging)
+        self.opcthread = opccoll(self.configjs, self.nodesjs, self.client, self.dbconn)
         self.opcthread.start()
         self.dbconn.writecom("opc", "Starting", "Thread Started", "")
         return
 
-#    def collect(self, isLogging): 
-#        try:
-#            if self.opcthread.isAlive():
-#                self.dbconn.writecom("opc", "Error", "Thread already running", "")
-#                return
-#        except Exception as e:
-#            self.isLogging = isLogging
-#            self.opcthread = opccoll(self.configjs, self.nodesjs, self.client, self.dbconn, self.isLogging)
-#            self.opcthread.start()
-#            self.dbconn.writecom("opc", "Starting", "Thread Started", str(e))
-#            return
+
         
-    def stopcollect(self):
+    def stopOpc(self):
         try:
             self.opcthread.isRunning = False
             self.dbconn.writecom("opc", "Stopping", "Thread Stopped", "")
@@ -100,6 +99,12 @@ class opccon:
     def getActValues(self):
         try:
             return self.opcthread.actValues
+        except Exception as e:
+            return str(e)
+
+    def getStaticValues(self):
+        try:
+            return self.opcthread.staticValues
         except Exception as e:
             return str(e)
 
